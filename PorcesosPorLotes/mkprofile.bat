9e022f4ae17efa6b0db8b448aa5723ccbdacaffa
amurillol06 <amurillol06@iescastelar.com>
2024-11-18 09:53:56 +0100

    Add files via upload

---
+++ PorcesosPorLotes/mkprofile.bat
@@ -0,0 +1,11 @@
+echo off
+rem Script de pruba
+rem Autor:Adrian Murillo
+rem Fecha: 18-11-2024
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\dam %USERPROFILE%\smr %USERPROFILE%\ceti
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\examen 
+mkdir %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\examen 
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\examen 
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\examen 
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\examen 
